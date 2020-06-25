9cf10e4eea2ecb9e20eb8a170fd387f323d2e5a3
vladimirmyshkovski <vladimirmyshkovski@gmaiol.com>
2020-06-25 14:15:49 +0400

    Add description to head in nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,7 @@ export default {
   mode: 'universal',
   head: {
     title: 'Lux Lighting',
+    description: 'Lux Lighting',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
