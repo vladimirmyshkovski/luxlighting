f49a78bc98db4b275ee7541de68f476a13f29360
vladimirmyshkovski <vladimirmyshkovski@gmaiol.com>
2020-07-02 18:18:19 +0400

    Update CatalogDropdown component

--- components/CatalogDropdown.vue
+++ components/CatalogDropdown.vue
@@ -1,22 +1,39 @@
 <template>
-  <div class="catalog__dropdown">
+  <div class="dropdown">
+    <div class="dropdown__title">КАТАЛОГ</div>
     <div class="tabs">
       <div v-for="(cat, index) in categories" :key="index" class="tab">
-        <input :id="`check-${index}`" type="checkbox" :value="cat.cat" />
-        <label class="tab__label" :for="`check-${index}`">
-          {{ cat.cat }}
-        </label>
-        <div class="tab__content">
-          <div
+        <input
+          :id="`check-${index}`"
+          type="checkbox"
+          :value="cat.cat"
+          @click="$emit('chooseCategory', cat.id)"
+        />
+        <label class="tab__label" :for="`check-${index}`">{{ cat.cat }}</label>
+        <ul class="tab__content">
+          <li
             v-for="(subcat, idx) in cat.subcats"
-            :key="idx"
-            @click="$emit('chooseCat', cat.id)"
+            :key="subcat.name"
+            @click="showDetails(subcat, idx)"
           >
-            <p>
+            <p :class="['subcat', { 'subcat-active': activeSubcat === index }]">
               {{ subcat.name }}
             </p>
-          </div>
-        </div>
+            <ul v-if="details" class="details">
+              <li
+                v-for="(detail, i) in subcat.details"
+                :key="detail"
+                :class="[
+                  'details__detail',
+                  { 'detail-active': activeProduct === i }
+                ]"
+                @click.stop="chooseProduct(subcat.name, i)"
+              >
+                - Длина: {{ detail }} мм
+              </li>
+            </ul>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
@@ -27,39 +44,58 @@ import categories from '../categories.json'
 export default {
   data() {
     return {
+      activeSubcat: null,
+      activeProduct: null,
+      details: false,
       categories
+    }
+  },
+  methods: {
+    showDetails(item, index) {
+      this.active = index
+      // eslint-disable-next-line no-prototype-builtins
+      if (item.hasOwnProperty('details') && !this.details) {
+        this.details = true
+      } else {
+        this.details = false
+      }
+    },
+    chooseProduct(item, index) {
+      this.activeProduct = index
+      this.$emit('chooseProduct', item)
     }
   }
 }
 </script>
 
-<style lang="scss" scoped>
-.catalog__dropdown {
-  width: 230px;
-  position: absolute;
-  top: 45px;
-  left: 0;
+<style lang="scss">
+.dropdown {
+  min-width: 300px;
+  text-transform: uppercase;
+
+  &__title {
+    margin-bottom: 10px;
+    padding-bottom: 10px;
+    border-bottom: 4px solid #f7c601;
+    color: #f7c601;
+    font-size: 24px;
+  }
 
   .tabs {
     overflow: hidden;
 
     .tab {
       width: 100%;
-      background-color: #523526;
       overflow: hidden;
 
       &__label {
-        padding: 10px;
+        padding: 10px 0;
         display: flex;
         justify-content: space-between;
-        border-bottom: 1px solid #835d49;
+        align-items: center;
         font-family: 'MyRiad Pro Semibold';
-        font-size: 12px;
+        font-size: 14px;
         cursor: pointer;
-
-        &:hover {
-          background-color: darken(#835d49, 10%);
-        }
 
         &::after {
           content: '\276F';
@@ -72,18 +108,29 @@ export default {
       }
       &__content {
         max-height: 0;
-        padding: 0;
-        background-color: #30180b;
-        font-size: 10px;
+        padding-left: 20px;
+        font-size: 12px;
         cursor: pointer;
         list-style-type: none;
+        border-bottom: 1px solid #fff;
 
-        p {
-          padding: 5px 10px;
-          border-bottom: 1px solid #523526;
+        .subcat {
+          padding: 2px 0;
+        }
 
-          &:hover {
-            background-color: darken(#30180b, 5%);
+        .subcat-active {
+          font-family: 'MyRiad Pro Semibold';
+        }
+
+        .details {
+          padding-left: 20px;
+          list-style-type: none;
+
+          &__detail {
+            padding: 2px 0;
+          }
+          .detail-active {
+            color: #f7c601;
           }
         }
       }
@@ -91,17 +138,17 @@ export default {
         position: absolute;
         opacity: 0;
         z-index: -1;
-
-        &:checked {
-          + .tab__label {
-            color: #f7c601;
-          }
-          + .tab__label::after {
-            transform: rotate(-90deg);
-          }
-          ~ .tab__content {
-            max-height: 100vh;
-          }
+      }
+      input:checked {
+        + .tab__label {
+          color: #f7c601;
+        }
+        + .tab__label::after {
+          transform: rotate(-90deg);
+        }
+        ~ .tab__content {
+          max-height: 100vh;
+          //padding: 15px;
         }
       }
     }
